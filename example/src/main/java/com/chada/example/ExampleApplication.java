eab318df3769328bee16134f3a2201a1c8594a01
Adrian <adrian.bielenik@gmail.com>
2025-03-30 23:29:25 +0200

    30.03

--- example/src/main/java/com/chada/example/ExampleApplication.java
+++ example/src/main/java/com/chada/example/ExampleApplication.java
@@ -1,5 +1,6 @@
 package com.chada.example;
 
+import com.chada.example.api.Client;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,6 +10,7 @@ public class ExampleApplication {
 	public static void main(String[] args) {
 
 		SpringApplication.run(ExampleApplication.class, args);
+		//Client.main();
 	}
 
 }
